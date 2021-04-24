3f91eeaf2fea3aba4ff0e446fd279cf5cc0ab42a
Zhixuan Yang <zhixuanyang@ZhixuandeMacBook-Pro.local>
2021-04-24 13:45:57 +0800

    Lab1 Finished

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -8,13 +8,13 @@ public class LeapYear {
      */
     public static boolean isLeapYear(int year){
         if (year % 400 == 0){
-            return True
+            return true;
         }
         else if (year % 4 == 0 & year % 100 != 0){
-            return True
+            return true;
         }
         else{
-            return False
+            return false;
         }
     }
 
