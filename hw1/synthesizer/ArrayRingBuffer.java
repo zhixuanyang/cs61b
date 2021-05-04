c8bfc834664bd0f935a67db54dec60529f134806
zhixuanyang <yzx72497@gmail.com>
2021-05-05 02:06:52 +0800

    updated hw1

---
+++ hw1/synthesizer/ArrayRingBuffer.java
@@ -0,0 +1,84 @@
+package synthesizer;
+import java.util.Iterator;
+public class ArrayRingBuffer<T> extends AbstractBoundedQueue<T> {
+    /* Index for the next dequeue or peek. */
+    private int first;            // index for the next dequeue or peek
+    /* Index for the next enqueue. */
+    private int last;
+    /* Array for storing the buffer data. */
+    private T[] rb;
+
+    /**
+     * Create a new ArrayRingBuffer with the given capacity.
+     */
+    public ArrayRingBuffer(int capacity) {
+        rb = (T[]) new Object[capacity];
+        first = 0;
+        last = 0;
+        fillCount = 0;
+        this.capacity = capacity;
+    }
+
+    /**
+     * Adds x to the end of the ring buffer. If there is no room, then
+     * throw new RuntimeException("Ring buffer overflow"). Exceptions
+     * covered Monday.
+     */
+    public void enqueue(T x) {
+        if (isFull()) {
+            throw new RuntimeException("Ring Buffer Overflow");
+        }
+        rb[last] = x;
+        last = (last + 1) % capacity();
+        fillCount += 1;
+    }
+
+    /**
+     * Dequeue oldest item in the ring buffer. If the buffer is empty, then
+     * throw new RuntimeException("Ring buffer underflow"). Exceptions
+     * covered Monday.
+     */
+    public T dequeue() {
+        if (isEmpty()) {
+            throw new RuntimeException("Ring Buffer Underflow");
+        }
+        T temp = rb[first];
+        rb[first] = null;
+        fillCount -= 1;
+        first = (first + 1) % capacity();
+        return temp;
+    }
+
+    /**
+     * Return oldest item, but don't remove it.
+     */
+    public T peek() {
+        return rb[first];
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new ArrayRingBufferIterator();
+    }
+
+    private class ArrayRingBufferIterator implements Iterator<T> {
+        private int start;
+        private int number;
+
+        ArrayRingBufferIterator() {
+            start = first;
+            number = 0;
+        }
+
+        public boolean hasNext() {
+            return number < capacity();
+        }
+
+        public T next() {
+            T temp = rb[start];
+            start = (start + 1) % capacity();
+            number += 1;
+            return temp;
+        }
+    }
+}
