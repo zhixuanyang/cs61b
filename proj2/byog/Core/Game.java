a9278ac6d0f8af11f639ed02f6a17ecb14ad9957
zhixuanyang <yzx72497@gmail.com>
2021-05-08 10:42:40 +0800

    finished phase1

--- proj2/byog/Core/Game.java
+++ proj2/byog/Core/Game.java
@@ -9,10 +9,6 @@ public class Game {
     /* Feel free to change the width and height. */
     public static final int WIDTH = 80;
     public static final int HEIGHT = 30;
-    private boolean setupMode = true;     // flag to check whether setup has been done
-    private boolean newGameMode = false; // flag to check whether a new game is gonna be generated
-    private boolean quitMode = false;
-    private String seedString = ""; // store input random seed numbers as String
     static Random random;
     /**
      * Method used for playing a fresh game. The game should start from the main menu.
@@ -32,74 +28,29 @@ public class Game {
      * @param input the input string to feed to your program
      * @return the 2D TETile[][] representing the state of the world
      */
-    private void processInput(String input) {
-
-        if (input == null) {
-            System.out.println("No input given.");
-            System.exit(0);
-        }
-
-        String first = Character.toString(input.charAt(0));
-        first = first.toLowerCase(); // normalize an input to lower case
-        processInputString(first);
-
-        if (input.length() > 1) {
-            String rest = input.substring(1);
-            processInput(rest); // recursive call until input ends
-        }
-
-    }
-
-    private void switchSetupMode() {
-        setupMode = !setupMode;
-    }
-
-    private void switchNewGameMode() {
-        newGameMode = !newGameMode;
-    }
-
-    private void switchQuitMode() {
-        quitMode = !quitMode;
-    }
-
-
-    private void processInputString(String first) {
-
-        if (setupMode) {      // when the setup hasn't been done
-            switch (first) {
-                case "n":       // new game gonna be generated
-                    switchNewGameMode();
-                    break;
-                case "s":       // setup a new game
-                    setupNewGame();
-                    break;
-                case "q":
-                    System.exit(0);
-                    break;
-                default:        // append next seed integer to seedString
-                    try {
-                        Long.parseLong(first);
-                        seedString += first;
-                    } catch (NumberFormatException e) { // exit program if input is invalid
-                        System.out.println("Invalid input given: " + first);
-                        System.exit(0);
-                    }
-                    break;
-            }
-        }
-    }
-
-    private void setupNewGame() {
-        random = new Random(Long.parseLong(seedString));
-    }
 
     public TETile[][] playWithInputString(String input) {
+        char[] chars = input.toCharArray();
         // and return a 2D tile representation of the world that would have been
         // drawn if the same inputs had been given to playWithKeyboard().
-        processInput(input);
         WorldGenerator wg = new WorldGenerator();
-        TETile[][] world = wg.initialworld();
-        TETile[][] finalWorldFrame = wg.playthegame(world);
+        TETile[][] world = new TETile[0][];
+        TETile[][] finalWorldFrame = new TETile[0][];
+        String temp = "";
+        long rand;
+        int loc = 1;
+        if (chars[0] == 'N' || chars[0] == 'n') {
+            world = wg.initialworld();
+        }
+        while (!Character.isAlphabetic(chars[loc])) {
+            temp += String.valueOf(chars[loc]);
+            loc += 1;
+        }
+        rand = Long.parseLong(temp);
+        random = new Random(rand);
+        if (chars[loc] == 'S' || chars[loc] == 's') {
+            finalWorldFrame = wg.playthegame(world);
+        }
         return finalWorldFrame;
     }
 }
