4e8a99135b1c77843643e58e05753ce23d059f38
zhixuanyang <yzx72497@gmail.com>
2021-05-14 01:38:12 +0800

    update hw2

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -28,27 +28,87 @@ public class Percolation {
         if (row >= size || col >= size || row < 0 || col < 0) {
             throw new IndexOutOfBoundsException("The input out of the range");
         }
-        set[row][col] = true;
-        numberofopen += 1;
+        if (!set[row][col]) {
+            set[row][col] = true;
+            numberofopen += 1;
+            if (row == 0 & col == 0) {
+                checkdown(row, col);
+                checkright(row, col);
+            } else if (row == size - 1 & col == 0) {
+                checkright(row, col);
+                checkup(row, col);
+            } else if (row == 0 & col == size - 1) {
+                checkdown(row, col);
+                checkleft(row, col);
+            } else if (row == size -1 & col == size - 1) {
+                checkup(row, col);
+                checkleft(row, col);
+            } else if (row == 0) {
+                checkdown(row, col);
+                checkleft(row, col);
+                checkright(row, col);
+            } else if (row == size - 1) {
+                checkup(row, col);
+                checkleft(row, col);
+                checkright(row, col);
+            } else if (col == 0) {
+                checkdown(row, col);
+                checkup(row, col);
+                checkright(row, col);
+            } else if (col == size - 1) {
+                checkdown(row, col);
+                checkup(row, col);
+                checkleft(row, col);
+            } else {
+                checkdown(row, col);
+                checkup(row, col);
+                checkleft(row, col);
+                checkright(row, col);
+            }
+        }
+    }
+
+    public void checkup(int row, int col) {
         if (set[row - 1][col]) {
             disjointset.union(xyTo1D(row, col), xyTo1D(row - 1, col));
         }
+    }
+
+    public void checkdown(int row, int col) {
         if (set[row + 1][col]) {
             disjointset.union(xyTo1D(row, col), xyTo1D(row + 1, col));
         }
+    }
+
+    public void checkleft(int row, int col) {
         if (set[row][col - 1]) {
             disjointset.union(xyTo1D(row, col), xyTo1D(row, col - 1));
         }
+    }
+
+    public void checkright(int row, int col) {
         if (set[row][col + 1]) {
             disjointset.union(xyTo1D(row, col), xyTo1D(row, col + 1));
         }
     }
 
+
+
     public boolean isOpen(int row, int col) {
+        if (row >= size || col >= size || row < 0 || col < 0) {
+            throw new IndexOutOfBoundsException("The input out of the range");
+        }
         return set[row][col];
     }
 
     public int numberOfOpenSites() {
         return numberofopen;
+    }
+
+    public boolean isFull(int row, int col) {
+        if (row >= size || col >= size || row < 0 || col < 0) {
+            throw new IndexOutOfBoundsException("The input out of the range");
+        }
+        return false;
     }
 }
